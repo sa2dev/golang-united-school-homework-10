af681537a9f7265cf993c7f34d9941f0088e7588
Max Reshetnikov <maxim.a.reshetnikov@gmail.com>
2022-07-17 19:28:16 +0400

    homework-10 attempt 1

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -20,11 +21,42 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/bad", handleBad).Methods(http.MethodGet)
+	router.HandleFunc("/name/{PARAM}", handleNameParam).Methods(http.MethodGet)
+	router.HandleFunc("/data", handleDataParam).Methods(http.MethodPost)
+	router.HandleFunc("/headers", handleHeaders).Methods(http.MethodPost)
+	router.NotFoundHandler = http.HandlerFunc(notFound)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handleHeaders(w http.ResponseWriter, r *http.Request) {
+	header := r.Header
+	a, _ := strconv.Atoi(header.Get("a"))
+	b, _ := strconv.Atoi(header.Get("b"))
+	w.Header().Add("a+b", strconv.Itoa(a+b))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func handleDataParam(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	fmt.Fprintf(w, "I got message:\n%s", body)
+}
+
+func handleNameParam(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	param := params["PARAM"]
+	fmt.Fprintf(w, "Hello, %s!", param)
+}
+
+func handleBad(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
 }
 
 //main /** starts program, gets HOST:PORT param and calls Start func.
